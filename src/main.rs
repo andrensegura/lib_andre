fa51ad1e332cef1f651ae84ba6a57344c562796c
Andre Segura <andrensegura@gmail.com>
2015-10-13 13:25:00 -0400

    Updated examples.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,25 @@
 extern crate lib_andre;
 
-use lib_andre::io;
+use lib_andre::{io,os};
+//use std::env; //for cmdline args
 
 fn main() {
+    //example for prompt()
     let name = io::prompt("Name: ").unwrap();
-    println!("Your name is {}.", name.trim());
     
+    //example for print_file()
     io::print_file("./test.txt").unwrap();
+
+    //example for is_valid_user()
+    //if you want to use cmdline args:
+    //  let args: Vec<_> = env::args().collect();
+    //  let user = args[1].clone();
+
+    println!("-- Checking user ...");
+
+    if os::is_valid_user(name.as_ref()) {
+        println!("-- User is valid: {}", name);
+    } else {
+        println!("-- User in invalid: {}", name);
+    }
 }
