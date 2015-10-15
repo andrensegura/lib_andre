79462f5d10ff86d02d80d9d987e160f272e5d05c
Andre Segura <andrensegura@gmail.com>
2015-10-14 20:09:10 -0400

    print_file no longer prints the file on it's own
    print_file now only returns the string it produces. You must now decide what to do with the string. This is to prevent unexpected behavior.

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -14,13 +14,11 @@ pub fn prompt(ps: &str) -> Result<String, io::Error> {
     Ok(result)
 }
 
-//take a file name as a string and prints the contents of the file
-//if it exists.
+//take a file name as a string and returns the contents of the file
+//as a string if it exists.
 pub fn print_file(file_name: &str) -> Result<String, Box<Error>> {
     let mut f = try!(File::open(file_name));
     let mut s = String::new();
     try!(f.read_to_string(&mut s));
-    println!("{}", s);
-
     Ok(s)
 }
